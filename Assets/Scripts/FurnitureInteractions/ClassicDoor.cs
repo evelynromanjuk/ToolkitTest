c4f4eb27ba70a3479fb813df4626d77ec422262a
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-12 23:01:47 +0200

    Began: DoorOpen onSelected Script
    - new script + XR Simple Interactable on test door

---
+++ Assets/Scripts/FurnitureInteractions/ClassicDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClassicDoor : MonoBehaviour
+{
+    //private XRSimpleInteractable simpleInteractable = null;
+    [SerializeField] private Animator myDoor = null;
+    private bool isOpen = false;
+
+    private void Awake()
+    {
+        //simpleInteractable = GetComponent <XRSimpleInteractable>();
+
+        //simpleInteractable.activated.AddListener(MoveDoor);
+    }
+
+    public void SetActive (bool value)
+    {
+        if (value)
+        {
+            myDoor.Play("DoorOpen", 0, 0.0f);
+            Debug.Log("Door Open");
+        }
+        else
+        {
+            myDoor.Play("DoorClose", 0, 0.0f);
+            Debug.Log("Door Closed");
+        }
+
+        Debug.Log("You Clicked");
+    }
+
+    public void SetActive2()
+    {
+        if (!isOpen)
+        {
+            myDoor.Play("DoorOpen", 0, 0.0f);
+            isOpen = true;
+        }
+        else
+        {
+            myDoor.Play("DoorClose", 0, 0.0f);
+            isOpen = false;
+            Debug.Log("Close Animation");
+        }
+
+        Debug.Log("You Clicked");
+    }
+}
