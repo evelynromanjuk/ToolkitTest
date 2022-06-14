ffe9d29618e09d816cfd29c4831ec5953df40053
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-14 18:03:46 +0200

    Updated: Substance Machine + Lever
    Interactable lever on click; Scan object integrated in substance machine

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+
+    public bool isOpen = false;
+
+    public void SetActive()
+    {
+        if (!isOpen)
+        {
+            myDoor.Play("Open", 0, 0.0f);
+            isOpen = true;
+        }
+        //else
+        //{
+        //    myDoor.Play("Close", 0, 0.0f);
+        //    isOpen = false;
+        //    Debug.Log("Close Animation");
+        //}
+
+        Debug.Log("You Clicked");
+    }
+
+}
