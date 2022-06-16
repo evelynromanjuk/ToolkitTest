0a8a9cdb8996022278ee17aba527da6fad67b004
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-16 17:30:19 +0200

    Started: Spawn Substance Ball

---
+++ Assets/Scripts/SpawnBall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void spawnBall()
+    {
+        GameObject b = Instantiate(ballPrefab) as GameObject;
+        b.transform.position = new Vector3(2.39f, 1.5f, 4.68f);
+
+        Debug.Log("Spawn Ball");
+    }
+}
