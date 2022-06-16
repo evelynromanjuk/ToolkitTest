61ff361e3e78eed8243fe15e0d4e01799b46b38b
DehV <david.brunschier@gmail.com>
2022-06-16 17:29:04 +0200

    Mail Programm

---
+++ Assets/Hacker UI/Scripts/MailManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MailManager : MonoBehaviour
+{
+    public GameObject MailScroll;
+    public GameObject MailPrefab;
+    public GameObject mailTextPanel;
+    
+    public void CreateMail(MailScriptableObject mail)
+    {
+        GameObject Email = Instantiate(MailPrefab, MailScroll.transform);
+        Email.transform.SetAsFirstSibling();
+        Email.GetComponent<Mail>().baseMail = mail;
+        Email.GetComponent<Mail>().mailTextPanel = mailTextPanel;
+        Email.transform.Find("Betreff").GetComponent<TMP_Text>().text = mail.Betreff;
+        Email.transform.Find("Absender").GetComponent<TMP_Text>().text = mail.Absender;
+    }
+}
---
+++ Assets/Hacker UI/Scripts/ResetBackgrounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBackgrounds : MonoBehaviour
+{
+    public GameObject Background;
+    public List<GameObject> backgrounds;
+
+    public void Awake()
+    {
+        for (int i = 1; i < Background.transform.childCount; i++)
+        {
+            backgrounds.Add(Background.transform.GetChild(i).gameObject);
+        }
+        
+    }
+
+    public void resetBackgrounds()
+    {
+        foreach (GameObject background in backgrounds)
+        {
+            background.SetActive(false);
+        }
+    }
+}
