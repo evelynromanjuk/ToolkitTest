3f44d9ab96ff009e0e96d53460af0b2e8958e1e6
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-23 21:18:02 +0200

    Started: Flowing Liquid
    Flowing liquid with blend shapes, transforms on click

---
+++ Assets/LiquidFlow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidFlow : MonoBehaviour
+{
+
+    private float size = 0.0f;
+
+    public void openTube()
+    {
+        InvokeRepeating("Scale", 0.0f, 0.02f);
+    }
+
+    void Scale()
+    {
+        if (size >= 100.0f)
+        {
+            CancelInvoke("Scale");
+        }
+
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(0, size++);
+    }
+}
