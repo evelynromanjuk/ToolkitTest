4eb559a95b43772fbd4c23079833173d4490c557
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-28 00:31:29 +0200

    URP installed + Progress Shader

---
+++ Assets/TankFill.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankFill : MonoBehaviour
+{
+
+    [SerializeField] FluidCompositionManager FluidCompositionManager;
+    public Material tankMaterial;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        tankMaterial = GetComponent<MeshRenderer>().material;
+        tankMaterial.SetFloat("_FillRate", -1);
+        Debug.Log("Applied Material: " + tankMaterial);
+    }
+    void Start()
+    {
+        FluidCompositionManager.SubscribeFluidAmountChanged(OnFluidAmountChanged);
+
+    }
+
+    void OnFluidAmountChanged(float percentage)
+    {
+        float conversion1 = percentage / 100;
+        float fillRate = conversion1 - 0.5f;
+
+        tankMaterial.SetFloat("_FillRate", fillRate);
+        Debug.Log("Current Fill Rate: " + fillRate);
+
+    }
+}
