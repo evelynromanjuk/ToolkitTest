072518f0160febf29410a983d3bc11f0e5f663d0
evelynromanjuk <evelyn.romanjuk@gmail.com>
2022-06-29 21:20:13 +0200

    AnalyzerRoom
    - new materials
    - added parts
    - socket interactors
    - checks if part correct

---
+++ Assets/Scripts/AnalyzerPuzzle/CheckSocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CheckSocket : MonoBehaviour
+{
+    public GameObject analyzerPart;
+    private XRSocketInteractor socket;
+
+    Action<bool> PartInsertedEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+    }
+
+    public void SubscribePartInsertedEvent(Action<bool> method)
+    {
+        PartInsertedEvent += method;
+    }
+
+    public void checkPart()
+    {
+        bool partIsCorrect = false;
+
+        IXRSelectInteractable insertedPart = socket.GetOldestInteractableSelected();
+
+        if (analyzerPart.transform.name == insertedPart.transform.name)
+        {
+            partIsCorrect = true;
+            Debug.Log("This is the correct part");
+        }
+        else
+        {
+            Debug.Log("This is NOT the correct part");
+        }
+
+
+
+    }
+}
