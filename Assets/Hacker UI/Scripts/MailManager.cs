43a2d30512a0a2d711f28dbc3764103e494c460d
DehV <david.brunschier@gmail.com>
2022-06-30 22:59:03 +0200

    Candy Analyzer

--- Assets/Hacker UI/Scripts/MailManager.cs
+++ Assets/Hacker UI/Scripts/MailManager.cs
@@ -8,7 +8,12 @@ public class MailManager : MonoBehaviour
     public GameObject MailScroll;
     public GameObject MailPrefab;
     public GameObject mailTextPanel;
-    
+    public AudioSource youveGotMail;
+
+    private void Awake()
+    {
+        youveGotMail = GetComponent<AudioSource>();
+    }
     public void CreateMail(MailScriptableObject mail)
     {
         GameObject Email = Instantiate(MailPrefab, MailScroll.transform);
@@ -17,5 +22,6 @@ public class MailManager : MonoBehaviour
         Email.GetComponent<Mail>().mailTextPanel = mailTextPanel;
         Email.transform.Find("Betreff").GetComponent<TMP_Text>().text = mail.Betreff;
         Email.transform.Find("Absender").GetComponent<TMP_Text>().text = mail.Absender;
+        youveGotMail.Play();
     }
 }
---
+++ Assets/Hacker UI/Scripts/UI_Fluid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Fluid : MonoBehaviour
+{
+    public Slider fillSlider;
+    [SerializeField] FluidCompositionManager FluidCompositionManager;
+
+    private void Awake()
+    {
+        fillSlider = GetComponent<Slider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        FluidCompositionManager.SubscribeFluidAmountChanged(OnFluidAmountChanged);
+        FluidCompositionManager.SubscribeCompositionCorrectEvent(OnFluidCorrect);
+    }
+
+    void OnFluidAmountChanged(float percentage)
+    {
+        fillSlider.value = percentage;
+    }
+
+    void OnFluidCorrect(bool correct)
+    {
+
+    }
+}
