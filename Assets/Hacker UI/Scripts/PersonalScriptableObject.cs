dec49836be043df39b457b9fa3f4b5d4e7b0c714
DehV <david.brunschier@gmail.com>
2022-07-07 09:10:50 +0200

    Personal Akte
    Personal Akte

---
+++ Assets/Hacker UI/Scripts/PersonalScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Personal", menuName = "Personal", order = 1)]
+public class PersonalScriptableObject : ScriptableObject
+{
+    public string Name;
+    public string text;
+}
---
+++ Assets/PersonalAkte.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PersonalAkte : MonoBehaviour
+{
+    public PersonalScriptableObject person;
+    public TMP_Text textfenster;
+
+
+    private void Awake()
+    {
+        GetComponentInChildren<TMP_Text>().text = person.Name;
+    }
+
+    public void OnClick()
+    {
+        textfenster.text = person.text;
+    }
+}
